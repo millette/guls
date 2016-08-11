fb55233cdb12d3f3ed851af059d128f54f165219
Robin Millette <robin@millette.info>
2016-08-11 17:42:41 -0400

    With successful unit tests.

---
+++ test.js
@@ -0,0 +1,20 @@
+'use strict'
+import test from 'ava'
+import fn from './'
+
+test('byTitle, normal', async t => {
+  const result = await fn.byTitle('Communauté WordPress Montréal')
+  t.is(result.pageid, 555)
+})
+
+test('byTitle, redir', async t => {
+  const result = await fn.byTitle('Montreal WordPress Community')
+  t.is(result.pageid, 555)
+})
+
+test('byTitle, nope', async t => await t.throws(fn.byTitle('Mon666treal WordPress Community'), 'Not found.'))
+
+/*
+fn.tablePage(() === fetchTablePage()
+fn() === fetchAll()
+*/

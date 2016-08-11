de9935fd1bee26b45fc751845713cde980b5099f
Robin Millette <robin@millette.info>
2016-08-11 15:00:51 -0400

    Fetch community page and linked pages.

--- cli.js
+++ cli.js
@@ -4,31 +4,17 @@
 
 const wi = require('./')
 
-/*
-wi('Communautés d\'utilisateurs et de développeurs de logiciels libres au Québec', true)
-  .then((x) => {
-    console.log(JSON.stringify(x, null, ' '))
+var done = false
+wi.wrk2('Communautés d\'utilisateurs et de développeurs de logiciels libres au Québec')
+  .then((out2) => {
+    done = true
+    console.log(JSON.stringify(out2, null, ' '))
   })
-  .catch((e) => console.error(e))
-*/
+  .catch((e) => {
+    console.error('EE:', e))
+    done = true
+  }
 
-const wrk = (pages, allDone) => {
-  const out = []
-  let cnt = 0
-  pages.forEach((page) => {
-    wi.fetchByTitle(page)
-      .then((x) => {
-        out.push(x)
-        if (++cnt === pages.length) { allDone(out) }
-      })
-      .catch((e) => {
-        console.error(page, e)
-        if (++cnt === pages.length) { allDone(out) }
-      })
-  })
-}
-
-wrk(
-  ['Mozilla Québec', 'DebianQuebec'],
-  (out2) => console.log('tadam', JSON.stringify(out2, null, ' '))
-)
+const i = setInterval(() => {
+  if (done) { clearInterval(i) }
+}, 300)

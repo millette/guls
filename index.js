6d64fa0bee0136703e7032d235bf98eeec9b4a44
Robin Millette <robin@millette.info>
2016-08-11 14:59:34 -0400

    Resolve redirects.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const RLP = require('rate-limit-promise')
 
 const rateLimter = new RLP(6, 6000)
 
-const fetchByTitle = (title) => rateLimter()
+const fetchByTitle = (title, redir) => rateLimter()
   .then(got.bind(null, `http://wiki.facil.qc.ca/api.php?format=json&action=query&titles=${title}&prop=contributors|fileusage|info|pageprops|redirects|revisions`, { json: true }))
   .then((x) => x.body.query)
   .then((x) => {
@@ -20,9 +20,13 @@ const fetchByTitle = (title) => rateLimter()
     return Promise.all([Object.assign(x, d), got(`http://wiki.facil.qc.ca/api.php?format=json&action=parse&pageid=${pageids[0]}&prop=wikitext`, { json: true })])
   })
   .then((x) => {
-    const ret = x[0]
-    ret.wikitext = x[1].body.parse.wikitext['*']
-    return ret
+    if (x[1].body.parse.wikitext['*'].indexOf('#REDIRECT ')) {
+      const ret = x[0]
+      ret.wikitext = x[1].body.parse.wikitext['*']
+      return ret
+    } else {
+      return fetchByTitle(x[1].body.parse.wikitext['*'].slice(12, -2))
+    }
   })
 
 const parseHeader = (wikitext) => {
@@ -55,6 +59,25 @@ const fetchTablePage = (page, keepWikitext) => fetchByTitle(page)
     return x
   })
 
+const wrk = (pages) => new Promise((resolve, reject) => {
+  const out = []
+  pages.forEach((page) => {
+    fetchByTitle(page)
+      .then((x) => {
+        out.push(x)
+        if (out.length === pages.length) { resolve(out) }
+      })
+      .catch((e) => {
+        out.push({ error: 404, page: page })
+        if (out.length === pages.length) { resolve(out) }
+      })
+  })
+})
+
+const wrk2 = (page) => fetchTablePage(page)
+  .then((x) => Promise.all([x, wrk(x.rows.map((y) => y.Nom.slice(2, -2)))]))
+
+fetchTablePage.wrk2 = wrk2
 fetchTablePage.fetchByTitle = fetchByTitle
 
 module.exports = fetchTablePage
